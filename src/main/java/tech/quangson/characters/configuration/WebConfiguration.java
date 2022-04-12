ae42351e3b735c06eb97b971e5e5f3e796ae2b1e
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-04-12 14:15:11 -0600

    Add configuration class to allow CORS mapping to localhost:4200 (angular port)

---
+++ src/main/java/tech/quangson/characters/configuration/WebConfiguration.java
@@ -0,0 +1,16 @@
+package tech.quangson.characters.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    // enable cross-origin resource sharing with Angular
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+    }
+}
