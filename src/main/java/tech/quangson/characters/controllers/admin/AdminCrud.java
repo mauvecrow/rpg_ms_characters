d7db154db19603bd7ffd2d6f2fad461ad3585ecc
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-04-18 16:50:17 -0600

    Created a service to enable CRUD operations via REST - note: the implementation class, AdminCrudImpl, is NOT a managed bean - the impl class needs to be manually created due to the abstraction of the dao classes, which is a dependency for the class

---
+++ src/main/java/tech/quangson/characters/controllers/admin/AdminCrud.java
@@ -0,0 +1,15 @@
+package tech.quangson.characters.controllers.admin;
+
+import java.util.List;
+
+public interface AdminCrud<T> {
+
+    T create(T entity);
+    T read(int entityId);
+    T update(int entityId, T entity);
+    boolean delete(int entityId);
+
+    List<T> getAll();
+    T updateEntityId(T newEntity, T oldEntity);
+
+}
---
+++ src/main/java/tech/quangson/characters/controllers/admin/AdminCrudImpl.java
@@ -0,0 +1,59 @@
+package tech.quangson.characters.controllers.admin;
+
+import org.springframework.data.repository.CrudRepository;
+import tech.quangson.characters.data.AbstractEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminCrudImpl<T extends AbstractEntity> implements AdminCrud<T>{
+
+    private final CrudRepository<T, Integer> dao;
+
+    public AdminCrudImpl(CrudRepository<T, Integer> dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public T create(T entity) {
+        return dao.save(entity);
+    }
+
+    @Override
+    public T read(int entityId) {
+        return dao.findById(entityId).orElse(null);
+    }
+
+    @Override
+    public T update(int entityId, T entity) {
+        var search = dao.findById(entityId).orElse(null);
+        if(search == null){
+            return null;
+        }
+        var updated = updateEntityId(entity, search);
+        return dao.save(updated);
+    }
+
+    @Override
+    public boolean delete(int entityId) {
+        var search = dao.findById(entityId);
+        if(search.isPresent()){
+            dao.deleteById(entityId);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<T> getAll() {
+        List<T> list = new ArrayList<>();
+        dao.findAll().forEach(list::add);
+        return list;
+    }
+
+    @Override
+    public T updateEntityId(T newEntity, T oldEntity) {
+        newEntity.setPrimaryKey(oldEntity.getPrimaryKey());
+        return newEntity;
+    }
+}
