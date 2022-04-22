675f05194d58686cebb2ca9f21361b399995640b
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-04-22 12:37:39 -0600

    Update configuration to allow PUT operations - also ensure post, get, delete are allowed

--- src/main/java/tech/quangson/characters/configuration/WebConfiguration.java
+++ src/main/java/tech/quangson/characters/configuration/WebConfiguration.java
@@ -11,6 +11,8 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
