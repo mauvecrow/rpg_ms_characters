7ad4fa7fe9d8c710c8554ca7ce937dbd8136bd2c
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-04-22 15:37:50 -0600

    create admin controller for characters

---
+++ src/main/java/tech/quangson/characters/controllers/admin/AdminCharactersController.java
@@ -0,0 +1,47 @@
+package tech.quangson.characters.controllers.admin;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.quangson.characters.data.RpgCharactersDao;
+import tech.quangson.characters.data.RpgCharactersEntity;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/characters")
+public class AdminCharactersController extends AdminAbstractController<RpgCharactersEntity> {
+
+    public AdminCharactersController(RpgCharactersDao dao) {
+        super(new AdminCrudImpl<>(dao));
+    }
+
+    @GetMapping
+    public List<RpgCharactersEntity> getAllCharacters(){
+        return getAllEntities();
+    }
+
+    @PutMapping
+    public ResponseEntity<?> saveAll(@RequestBody List<RpgCharactersEntity> characters){
+        return updateAll(characters);
+    }
+
+    @GetMapping("/{characterId}")
+    public ResponseEntity<?> getCharacter(@PathVariable int characterId){
+        return getEntity(characterId);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createCharacter(@RequestBody RpgCharactersEntity newCharacter){
+        return createEntity(newCharacter);
+    }
+
+    @PutMapping("/{characterId}")
+    public ResponseEntity<?> updateCharacter(@PathVariable int characterId, @RequestBody RpgCharactersEntity existingCharacter){
+        return updateEntity(characterId, existingCharacter);
+    }
+
+    @DeleteMapping("/{characterId}")
+    public ResponseEntity<?> deleteCharacter(@PathVariable int characterId){
+        return deleteEntity(characterId);
+    }
+}
